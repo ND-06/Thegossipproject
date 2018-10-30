41c1c200c2ce29a248eef1b31c9176348c1def89
Natdenice <Clad1987@hotmail.com>
2018-10-30 14:55:31 +0100

    Last release

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_page/team', to: "static_page#team"
-  get 'static_page/contact', to: "static_page#contact"
+  get '/team', to: "static_page#team"
+  get '/contact', to: "static_page#contact"
   get '/', to: 'static_page#index'
   get 'welcome/:name', to: 'static_page#welcome'
   get 'gossip/:id', to: 'static_page#gossip'
